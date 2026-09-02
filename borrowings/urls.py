306282c7b595a1a2d5ef5883705fbff87d37c92c
Sergio-zt <fop.gso@gmail.com>
2026-09-02 11:50:11 -0400

    Configure URLs and routers for borrowings app

---
+++ borrowings/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import BorrowingViewSet
+
+# Create a router instance
+router = DefaultRouter()
+
+# Register our BorrowingViewSet with the router
+# This automatically generates all standard CRUD URLs and our custom 'return' action
+router.register(r"borrowings", BorrowingViewSet)
+
+# app_name helps Django namespace the URLs
+app_name = "borrowings"
+
+urlpatterns = [
+    # Include all generated URLs from the router
+    path("", include(router.urls)),
+]
